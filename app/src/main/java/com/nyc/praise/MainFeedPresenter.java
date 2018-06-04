a2f04d4766b58a76ed9290ef5e2252e3aef05828
Wayne Kellman <waynekellman@ac.c4q.nyc>
2018-06-03 22:14:43 -0400

    made anon adapter into class

--- app/src/main/java/com/nyc/praise/MainFeedPresenter.java
+++ app/src/main/java/com/nyc/praise/MainFeedPresenter.java
@@ -12,16 +12,8 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.gson.Gson;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by Wayne Kellman on 5/12/18.
@@ -31,7 +23,7 @@ public class MainFeedPresenter {
     MainFeed mainFeedContext;
 
 
-    private FirebaseRecyclerAdapter<PraiseModel, FireBaseViewHolder> praiseFireBaseRecyclerAdapter;
+    private PraiseFireBaseRecyclerAdapter praiseFireBaseRecyclerAdapter;
     private DatabaseReference databaseReferenceFeed;
     String currentLocation;
     private final String TAG = "MainFeedPresenter";
@@ -42,7 +34,7 @@ public class MainFeedPresenter {
     private String USER_NAME;
 
     // Declare the RecyclerView and the LinearLayoutManager first
-    private RecyclerView listView;
+    private RecyclerView recyclerView;
     private LinearLayoutManager mLayoutManager;
 
 
@@ -58,17 +50,16 @@ public class MainFeedPresenter {
 
         currentLocationTextView = view.findViewById(R.id.current_location);
         sendPraise = view.findViewById(R.id.write_praise);
-        sendPraise.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                WritePraiseFragment fragment = new WritePraiseFragment();
-                Bundle newPraiseBundle = new Bundle();
-                newPraiseBundle.putBoolean("NewPraise", true);
-                newPraiseBundle.putString(Constants.LOCATION, currentLocation);
-                fragment.setArguments(newPraiseBundle);
-                switchContent(fragment);
-            }
-        });
+        sendPraise.setOnClickListener(view -> initWritePraiseFrag());
+    }
+
+    private void initWritePraiseFrag() {
+        WritePraiseFragment fragment = new WritePraiseFragment();
+        Bundle newPraiseBundle = new Bundle();
+        newPraiseBundle.putBoolean("NewPraise", true);
+        newPraiseBundle.putString(Constants.LOCATION, currentLocation);
+        fragment.setArguments(newPraiseBundle);
+        switchContent(fragment);
     }
 
     private void setGPS(GPSHelper gpsHelper) {
@@ -78,13 +69,10 @@ public class MainFeedPresenter {
 
     @NonNull
     private GPSHelper.LocationInterface getLocationInterface() {
-        return new GPSHelper.LocationInterface() {
-            @Override
-            public void onSuccessLocation(String currentLocation) {
-                Log.d(TAG, "onSuccessLocation: ran");
-                MainFeedPresenter.this.currentLocation = currentLocation;
-                connectMainFeed(currentLocation);
-            }
+        return currentLocation -> {
+            Log.d(TAG, "onSuccessLocation: ran");
+            MainFeedPresenter.this.currentLocation = currentLocation;
+            connectMainFeed(currentLocation);
         };
     }
 
@@ -92,101 +80,32 @@ public class MainFeedPresenter {
         Log.d(TAG, "connectMainFeed: ran");
         currentLocationTextView.setText(currentLocation.replace(" County", ""));
         databaseReferenceFeed = FirebaseDatabase.getInstance().getReference();
-        praiseFireBaseRecyclerAdapter = new FirebaseRecyclerAdapter<PraiseModel, FireBaseViewHolder>(
-                PraiseModel.class, R.layout.main_feed_itemview, FireBaseViewHolder.class,
-                databaseReferenceFeed.child(Constants.FEED).child(currentLocation)) {
-            @Override
-            protected void populateViewHolder(FireBaseViewHolder viewHolder, final PraiseModel model, int position) {
 
-                viewHolder.OnBind(model);
-                ValueEventListener listenerForLikes = getLikeValueEventListener(model, currentLocation, viewHolder);
-
-                databaseReferenceFeed.child(Constants.FEED).child(currentLocation).child(model.getuId()).child(Constants.LIKES).addValueEventListener(listenerForLikes);
-
-                viewHolder.commentImg.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        CommentsFragment fragment = new CommentsFragment();
-                        String modelJsonString = new Gson().toJson(model);
-                        Bundle bundle = new Bundle();
-                        bundle.putString("jsonModel", modelJsonString);
-                        fragment.setArguments(bundle);
-                        switchContent(fragment);
-                    }
-                });
-            }
-        };
+        praiseFireBaseRecyclerAdapter = getPraiseFireBaseRecyclerAdapter(currentLocation);
+        praiseFireBaseRecyclerAdapter.setUSER_NAME(USER_NAME);
+        praiseFireBaseRecyclerAdapter.setMainFeedContext(mainFeedContext);
 
         mLayoutManager = new LinearLayoutManager(mainFeedContext.getActivity());
         mLayoutManager.setReverseLayout(true);
         mLayoutManager.setStackFromEnd(true);
-        listView = view.findViewById(R.id.main_feed_recycler);
-        listView.setAdapter(praiseFireBaseRecyclerAdapter);
-        listView.setLayoutManager(mLayoutManager);
+        recyclerView = view.findViewById(R.id.main_feed_recycler);
+        recyclerView.setAdapter(praiseFireBaseRecyclerAdapter);
+        recyclerView.setLayoutManager(mLayoutManager);
     }
 
     @NonNull
-    private ValueEventListener getLikeValueEventListener(final PraiseModel model, final String currentLocation, final FireBaseViewHolder viewHolder) {
-        return new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String likeCountSnapshot = dataSnapshot.getChildrenCount() + "";
-                Log.d(TAG, "onDataChange: likes count " + likeCountSnapshot);
-                Map<String, String> UsersWhoLiked = createMapOfLikesBYUser(dataSnapshot);
-
-                setLikeButtonEnabledStatus(UsersWhoLiked, currentLocation, model, viewHolder);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        };
+    private PraiseFireBaseRecyclerAdapter getPraiseFireBaseRecyclerAdapter(String currentLocation) {
+        return new PraiseFireBaseRecyclerAdapter(
+                PraiseModel.class,
+                R.layout.main_feed_itemview, FireBaseViewHolder.class,
+                databaseReferenceFeed.child(Constants.FEED).child(currentLocation));
     }
 
-    private void setLikeButtonEnabledStatus(Map<String, String> usersWhoLiked, final String currentLocation, final PraiseModel model, final FireBaseViewHolder viewHolder) {
-        Log.d(TAG, "setLikeButtonEnabledStatus: model " + model.getMessage());
-        Log.d(TAG, "setLikeButtonEnabledStatus: ran");
-        if (usersWhoLiked.containsKey(USER_NAME)) {
-            for (String namesInMap : usersWhoLiked.keySet()) {
-                Log.d(TAG, "setLikeButtonEnabledStatus: " + namesInMap);
-            }
-            Log.d(TAG, "setLikeButtonEnabledStatus: " + USER_NAME);
-            viewHolder.likeImg.setEnabled(false);
-        } else {
-            viewHolder.likeImg.setEnabled(true);
-
-            viewHolder.likeImg.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Log.d(TAG, "onClick: likeImg ran");
-                    addLikeToDatabase(new HashMap<String, Object>(), currentLocation, model);
-                    viewHolder.likeImg.setEnabled(false);
-                }
-            });
-        }
-    }
-
-    private void addLikeToDatabase(Map<String, Object> userLike, String currentLocation, PraiseModel model) {
-        userLike.put(USER_NAME, String.valueOf(System.currentTimeMillis()));
-        databaseReferenceFeed.child(Constants.FEED).child(currentLocation).child(model.getuId()).child(Constants.LIKES).updateChildren(userLike);
-    }
-
-    @NonNull
-    private Map<String, String> createMapOfLikesBYUser(DataSnapshot dataSnapshot) {
-        Log.d(TAG, "createMapOfLikesBYUser: ran");
-        Map<String, String> UsersWhoLiked = new HashMap<>();
-        for (DataSnapshot likeFromDB : dataSnapshot.getChildren()) {
-            UsersWhoLiked.put(likeFromDB.getKey(), likeFromDB.getValue().toString());
-        }
-        return UsersWhoLiked;
-    }
 
     public void switchContent(Fragment fragment) {
 
-
         if (mainFeedContext.getActivity() instanceof MainScreenActivity) {
-            FragmentNavigater homeActivity = (FragmentNavigater) this.mainFeedContext.getActivity();
+            FragmentNavigator homeActivity = (FragmentNavigator) this.mainFeedContext.getActivity();
             homeActivity.SwitchFragment(fragment);
         }
 
